a36e634828a79aec5f547158d9f1e2d1a09ea856
jmmarin <jan.marien@ugent.be>
2020-05-20 16:25:51 +0200

    cleanup

--- demo/scripts/graphs.js
+++ demo/scripts/graphs.js
@@ -1,22 +1,37 @@
-function createTweetCountGraph(container_bars, container_lines) {
+var graphWidth = 170
+var graphHeight = 130
+var paddingLeft = 20
+
+/**
+ * 
+ * @param {string} containerBarsId 
+ * @param {string} containerLinesId 
+ */
+function createTweetCountGraph(containerBarsId, containerLinesId) {
   d3.csv('../data/tweets_per_day.csv').then(function(data){
-    var svg = container_bars.append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
-    var svg2 = container_lines.append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
+    var barSpacing = 1
+    var svg = d3.select(containerBarsId).append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
+    var svg2 = d3.select(containerLinesId).append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
     var bars = svg.selectAll('rect').data(data).enter();
     var numDays = data.length;
     var max = d3.max(data, function(d){ return +d['counts'] })
-    var yScale = d3.scaleLinear().domain([max, 0]).range([0, 130]);
-    var xScale = d3.scaleTime().domain(d3.extent(data, function(d) {
+    var yScale = d3.scaleLinear().domain([max, 0]).range([0, graphHeight]);
+    var domain = d3.extent(data, function(d) {
       return new Date(d['date'])
-    })).range([20, 190]);
+    })
+
+    // Add two extra days to the domain to have nicer axis
+    domain[0] = domain[0].setDate(domain[0].getDate() - 1)
+    domain[1] = domain[1].setDate(domain[1].getDate() + 1)
+    var xScale = d3.scaleTime().domain(domain).range([paddingLeft + (graphWidth/numDays - barSpacing)/2, graphWidth+paddingLeft - (graphWidth/numDays - barSpacing)/2]);
     
     var xAxis = d3.axisBottom().scale(xScale).ticks(14);
     var yAxis = d3.axisLeft().scale(yScale).ticks(20);
-    svg.append('g').attr('class','axis').attr("transform", "translate(0,150)").call(xAxis).selectAll('text').attr('y',0).attr('x', 15).attr("transform", "rotate(90)")
-    svg.append('g').attr('class','axis').style("font", "5px times").attr("transform", "translate(18,20)").call(yAxis)
+    svg.append('g').attr('class','axis').attr("transform", "translate(-"+barSpacing/2+","+graphHeight+")").call(xAxis).selectAll('text').attr('y',-2).attr('x', 15).attr("transform", "rotate(90)")
+    svg.append('g').attr('class','axis').style("font", "5px times").attr("transform", "translate(20,0)").call(yAxis)
     
-    svg2.append('g').attr('class','axis').attr("transform", "translate(0,150)").call(xAxis).selectAll('text').attr('y',0).attr('x', 15).attr("transform", "rotate(90)")
-    svg2.append('g').attr('class','axis').style("font", "5px times").attr("transform", "translate(18,20)").call(yAxis)
+    svg2.append('g').attr('class','axis').attr("transform", "translate(-"+barSpacing/2+",130)").call(xAxis).selectAll('text').attr('y',0).attr('x', 15).attr("transform", "rotate(90)")
+    svg2.append('g').attr('class','axis').style("font", "5px times").attr("transform", "translate(20,0)").call(yAxis)
 
     svg2.append("path")
     .datum(data)
@@ -24,19 +39,19 @@ function createTweetCountGraph(container_bars, container_lines) {
     .attr("stroke", "rgb(94, 204, 123)")
     .attr("stroke-width", 1)
     .attr("d", d3.line()
-      .x(function(d, i) { return xScale(new Date(d['date'])) })
-      .y(function(d) { return yScale(d['counts']) + 20
+      .x(function(d) { return xScale(new Date(d['date'])) })
+      .y(function(d) { return yScale(d['counts'])
      }).curve(d3.curveMonotoneX)
     )
 
     var rects = bars.append('rect')
-      .attr('x', (data, i) => {
-      return xScale(new Date(data['date']));
-    }).attr('y', (data, i) => {
-      return yScale(data['counts'])+20
-    }).attr('height', (data, i) => {
+      .attr('x', (data) => {
+      return xScale(new Date(data['date'])) - (graphWidth/numDays - barSpacing)/2;
+    }).attr('y', (data) => {
+      return yScale(data['counts'])
+    }).attr('height', (data) => {
       return 130 - yScale(data['counts'])
-    }).attr('width', (180 / numDays)-1)
+    }).attr('width', (170 / numDays) - barSpacing)
     .style('fill','rgb(94, 204, 123)');
           
     rects.on("mouseover", function(d){
@@ -58,7 +73,7 @@ function createTweetCountGraph(container_bars, container_lines) {
 }
 
 
-function createKeywordGraph(container) {
+function createKeywordGraph(containerId) {
     keywords = []
     d3.csv('../data/keyword_count.csv').then(function(data){
         data.sort(function(x, y) {
@@ -68,39 +83,42 @@ function createKeywordGraph(container) {
         keywords.push(d['keyword']);
       });
 
-      var svg = container.append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
+      var barSpacing = 5
+      var svg = d3.select(containerId).append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
       var bars = svg.selectAll('rect').data(data).enter();
       var max = d3.max(data, function(d){ return +d['count'] })
-      var yScale = d3.scaleLinear().domain([max, 0]).range([0, 130]);
-      var xScale = d3.scaleBand().domain(keywords).range([20, 190]);
+      var yScale = d3.scaleLinear().domain([max, 0]).range([0, graphHeight]);
+      var xScale = d3.scaleBand().domain(keywords).range([paddingLeft, graphWidth+paddingLeft]);
       var yAxis = d3.axisLeft().scale(yScale).ticks(10);
       var xAxis = d3.axisBottom().scale(xScale)
-      svg.append('g').attr('class','axis').style("font", "4px times").attr("transform", "translate(-1,150)").call(xAxis).selectAll('text').attr('y',5).attr('x', 10).attr("transform", "rotate(45)").style("font", '4px sans-serif')
+      svg.append('g').attr('class','axis').style("font", "4px times").attr("transform", "translate(-1,130)").call(xAxis).selectAll('text').attr('y',5).attr('x', 10).attr("transform", "rotate(45)").style("font", '4px sans-serif')
 
-      svg.append('g').attr('class','axis').style("font", "4px times").attr("transform", "translate(19,20)").call(yAxis)
+      svg.append('g').attr('class','axis').style("font", "4px times").attr("transform", "translate(19,0)").call(yAxis)
     
       bars.append('rect')
-        .attr('x', (data, i) => {
+        .attr('x', (data, _) => {
         return xScale(data['keyword']); 
-      }).attr('y', (data, i) => {
-        return yScale(data['count'])+20
-      }).attr('height', (data, i) => {
+      }).attr('y', (data, _) => {
+        return yScale(data['count'])
+      }).attr('height', (data, _) => {
         return 130 - yScale(data['count'])
-      }).attr('width', (180 / data.length)-5)
+      }).attr('width', (180 / data.length)-barSpacing)
       .style('fill','rgb(94, 204, 123)')
       
-      data.forEach(data => {
+      data.forEach(d => {
         svg.append('text')
-        .text(data['count'])
-        .attr('x', xScale(data['keyword']))
-        .attr('y', yScale(data['count']) + 18)
-        .style("font", '8px sans-serif')
+        .text(d['count'])
+        .attr('x', xScale(d['keyword']) + ((180 / data.length)-barSpacing)/2)
+        .attr('y', 129)
+        .attr('text-anchor', 'middle')
+        .style("font", '6px sans-serif')
       })
     })
 }
 
-function createInfectionsGraph(container) {    
+function createInfectionsGraph(containerId) {    
     covid = {}
+    barSpacing = 0.5
     d3.csv('data/COVID19BE_CASES_AGESEX.csv', function(d) {
       if (d['DATE'] in covid)
         covid[d['DATE']] = { date: d['DATE'], count: covid[d['DATE']]['count'] + parseInt(d['CASES'], 0) }
@@ -110,29 +128,32 @@ function createInfectionsGraph(container) {
       delete covid['NA']
       covid_data = Object.values(covid)
 
-      var svg = container.append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
+      var svg = d3.select(containerId).append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
       var bars = svg.selectAll('rect').data(covid_data).enter();
       var numDays = covid_data.length;
       var max = d3.max(covid_data, function(d){ return +d['count'] })
-      var yScale = d3.scaleLinear().domain([max, 0]).range([0, 130]);
-      var xScale = d3.scaleTime().domain(d3.extent(covid_data, function(d) {
+      var yScale = d3.scaleLinear().domain([max, 0]).range([0, graphHeight]);
+      var domain = d3.extent(covid_data, function(d) {
         return new Date(d['date'])
-      })).range([20, 190]);
+      })
+      domain[0] = domain[0].setDate(domain[0].getDate() - 1)
+      domain[1] = domain[1].setDate(domain[1].getDate() + 1)
+      var xScale = d3.scaleTime().domain(domain).range([paddingLeft + (graphWidth/numDays - barSpacing)/2, graphWidth+paddingLeft - (graphWidth/numDays - barSpacing)/2]);
       
-      var xAxis = d3.axisBottom().scale(xScale).ticks(14);
+      var xAxis = d3.axisBottom().scale(xScale).ticks(5).tickSizeOuter(0);
       var yAxis = d3.axisLeft().scale(yScale).ticks(10);
       
-      svg.append('g').attr('class','axis').attr("transform", "translate(0,150)").call(xAxis).selectAll('text').attr('y',0).attr('x', 15).attr("transform", "rotate(90)")
-      svg.append('g').attr('class','axis').style("font", "4px times").attr("transform", "translate(19,20)").call(yAxis)
+      svg.append('g').attr('class','axis').attr("transform", "translate(-"+barSpacing/2+","+graphHeight+")").call(xAxis).selectAll('text').attr('y',-2).attr('x', 15).attr("transform", "rotate(90)")
+      svg.append('g').attr('class','axis').style("font", "4px times").attr("transform", "translate(20.5,0)").call(yAxis)
 
       var rects = bars.append('rect')
-        .attr('x', (data, i) => {
-        return xScale(new Date(data['date'])); 
-      }).attr('y', (data, i) => {
-        return yScale(data['count']) + 20
-      }).attr('height', (data, i) => {
+        .attr('x', (data, _) => {
+        return xScale(new Date(data['date'])) - (graphWidth/numDays - barSpacing)/2; 
+      }).attr('y', (data, _) => {
+        return yScale(data['count'])
+      }).attr('height', (data, _) => {
         return 130 - yScale(data['count'])
-      }).attr('width', (180 / numDays) - 0.5)
+      }).attr('width', (graphWidth / numDays) - barSpacing)
       .style('fill','rgb(94, 204, 123)')
 
 
@@ -154,7 +175,11 @@ function createInfectionsGraph(container) {
     })
 }
 
-function createDeathsGraph(container) {
+/**
+ * 
+ * @param {string} containerId Id of the div container which will contain the graph
+ */
+function createDeathsGraph(containerId) {
   covid_deaths   = {}
   d3.csv('data/COVID19BE_MORT.csv', function(d) {
     if (d['DATE'] in covid_deaths)
@@ -165,30 +190,34 @@ function createDeathsGraph(container) {
     delete covid_deaths['NA']
     covid_deaths_filtered = Object.values(covid_deaths)
 
-    var svg = container.append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
+    var barSpacing = 0.5
+    var svg = d3.select(containerId).append('svg').attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 200 200").classed("svg-content", true);
     var bars = svg.selectAll('rect').data(covid_deaths_filtered).enter();
     var numDays = covid_deaths_filtered.length;
     var max = d3.max(covid_deaths_filtered, function(d){ return +d['count'] })
     var yScale = d3.scaleLinear().domain([max, 0]).range([0, 130]);
-    var xScale = d3.scaleTime().domain(d3.extent(covid_deaths_filtered, function(d) {
+    var domain = d3.extent(covid_deaths_filtered, function(d) {
       return new Date(d['date'])
-    })).range([200/numDays/2 + 18, 200 - 200/numDays/2]);
-    
+    })
+    domain[0] = domain[0].setDate(domain[0].getDate() - 1)
+    domain[1] = domain[1].setDate(domain[1].getDate() + 1)
+    var xScale = d3.scaleTime().domain(domain).range([paddingLeft + (graphWidth/numDays - barSpacing)/2, graphWidth+paddingLeft - (graphWidth/numDays - barSpacing)/2]);
+
     var xAxis = d3.axisBottom().scale(xScale).ticks(17).tickSize(4);
     var yAxis = d3.axisLeft().scale(yScale).ticks(10);
     
-    svg.append('g').attr('class','axis').attr("transform", "translate(0,130)").call(xAxis).selectAll('text').attr("transform", "rotate(90)").attr('y',-2).attr('x', 16)
-    svg.append('g').attr('class','axis').style("font", "4px times").attr("transform", "translate(18,0)").call(yAxis)
+    svg.append('g').attr('class','axis').attr("transform", "translate(-"+barSpacing/2+","+graphHeight+")").call(xAxis).selectAll('text').attr("transform", "rotate(90)").attr('y',-2).attr('x', 16)
+    svg.append('g').attr('class','axis').style("font", "4px times").attr("transform", "translate("+paddingLeft+",0)").call(yAxis)
 
     var rects = bars.append('rect')
-      .attr('x', (data, i) => {
-      return xScale(new Date(data['date'])); 
-    }).attr('y', (data, i) => {
-      return yScale(data['count'])
-    }).attr('height', (data, i) => {
-      return 130 - yScale(data['count'])
-    }).attr('width', (190 / numDays) - 0.5)
-    .style('fill','rgb(94, 204, 123)')
+      .attr('x', (data, _) => {
+        return xScale(new Date(data['date'])) - (graphWidth/numDays - barSpacing)/2; 
+      }).attr('y', (data, _) => {
+        return yScale(data['count'])
+      }).attr('height', (data, _) => {
+        return 130 - yScale(data['count'])
+      }).attr('width', (graphWidth / numDays) - barSpacing)
+      .style('fill','rgb(94, 204, 123)')
 
 
     rects.on("mouseover", function(d){
@@ -209,14 +238,22 @@ function createDeathsGraph(container) {
   })
 }
 
+/**
+ * Loads the tweet location data and displays it on a leaflet map
+ * @param {string} mapName The id of the map div element
+ */
 function createLocationMap(mapName) {
+  
+  // Set center to Brussels and zoom level to 7
+  // Use OpenStreetMap tiles
   var map = L.map(mapName).setView([50.8549541, 4.3053504], 7);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Timeout to notify leaflet the map size has changed
+    // Timeout to notify leaflet the map size has changed, otherwise only a part of the map is visible
     setTimeout(function(){ map.invalidateSize()
+      // Load the tweet locations and add circle on the map for every location
       d3.csv('data/tweet_locations.csv', function(d) {
         L.circle([d['location_lat'], d['location_lon']], {
           color: '#3399ff',
